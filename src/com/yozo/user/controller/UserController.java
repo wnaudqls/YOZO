a723506def0aa3affe4919c4dd52f9aacfbe60ce
LHSEUNGG <59066588+LHSEUNGG@users.noreply.github.com>
2020-07-24 10:17:10 +0900

    seung - 07-24 sqlMapConfig userController

--- src/com/yozo/user/controller/UserController.java
+++ src/com/yozo/user/controller/UserController.java
@@ -1,40 +1,35 @@
 package com.yozo.user.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UserController
- */
-@WebServlet("/UserController")
+
+@WebServlet("/User.do")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public UserController() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		String command = request.getParameter("command");
+		
+		
+		if(command.equals("login")) {
+			response.sendRedirect("login.jsp");
+		}
 	}
 
 }
