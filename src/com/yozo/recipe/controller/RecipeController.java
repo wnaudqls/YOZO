a7a8c5f8ddcf0a568e28b980997dcf6836ffbfa2
hyunmoSung <49181503+shm1113@users.noreply.github.com>
2020-08-03 16:22:18 +0900

    병욱이형테스트

--- src/com/yozo/recipe/controller/RecipeController.java
+++ src/com/yozo/recipe/controller/RecipeController.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.yozo.recipe.biz.RecipeBiz;
 import com.yozo.recipe.dto.RecipeDto;
 
@@ -40,6 +42,15 @@ public class RecipeController extends HttpServlet {
 		if(command.equals("jsoninsert")) {
 			System.out.println("jsoninsert!!왔다");
 			String rson=request.getParameter("rson");
+			
+			JsonElement element=JsonParser.parseString(rson);
+					//어떤 형식일지를 알 수 없기 떄문에 어떤 JSON데이터 형태도 저장할 수 있는 JSONElement형태로 받아 준다 
+					//jsonelement는 json요소를 나타내는 클래스
+					//jsonparser클래스의parseString메소드로 JSON형태의 문자열을 JSON타입으로 변경해주고
+								//	처음 Json요소를 가져올 때 기본적으로 jsonelement형태로 받아온다 
+								//	문자열을 JSON형태로 바꿔준다
+								//지정된 JSON 문자열을 구문 분석 트리로 구문 분석
+			
 			List<RecipeDto> list=new ArrayList<RecipeDto>();
 		//	for(int i=0;i<rson.length;i++) {
 				/*String[] arr=rson[i].split(regex)*/
